a89b7d88eb9b5f9de596946158fed4b8aa7e23fe
mlucas2805 <mlucas.ml27@gmail.com>
2016-07-20 12:27:12 +0200

    Page contact modifs

--- site/agency/contact.php
+++ site/agency/contact.php
@@ -4,42 +4,10 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">Un petit message...</h2>
-                <h3 class="section-subheading text-muted"></h3>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div>
-                    <p> Pour toutes questions contacter moi :</p>
-                    mlucas92{at}live{point}fr
-                </div>
-<!--                <form name="sentMessage" id="contactForm" method="post" action="formulaire.php" novalidate>-->
-<!--                    <div class="row">-->
-<!--                        <div class="col-md-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <input type="text" class="form-control" placeholder="Votre Prénom & Nom *" id="name" name="name" tabindex="1" required data-validation-required-message="Merci d'entrer votre Nom et Prénom">-->
-<!--                                <p class="help-block text-danger"></p>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-md-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <input type="email" class="form-control" placeholder="Votre Email *" id="email" name="email" tabindex="2" required data-validation-required-message="Merci d'entrer votre adresse mail">-->
-<!--                                <p class="help-block text-danger"></p>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-md-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <textarea class="form-control" placeholder="Vous pouvez écrire votre message, ici. *" id="message" name="message" tabindex="3" required data-validation-required-message="Merci de remplir la partie message"></textarea>-->
-<!--                                <p class="help-block text-danger"></p>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="clearfix"></div>-->
-<!--                        <div class="col-lg-12 text-center">-->
-<!--                            <div id="success"></div>-->
-<!--                            <button type="submit" name="envoi" class="btn btn-xl">Envoyer</button>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </form>-->
+                <p class="contact">Pour toutes questions contacter moi :                    mlucas92{at}live{point}fr<br>
+                    <br>
+                    ou suivez-moi : <a href="https://fr.linkedin.com/in/marie-lucas-1b6628bb"> Linkedin</a>  || <a href="https://github.com/mlucas2805"> GitHub</a> || <a href="https://twitter.com/GreenGeekMl27"> Twitter</a>
+                </p>
             </div>
         </div>
     </div>
