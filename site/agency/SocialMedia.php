d83eb6d0b597f7963caf305631399ccbf9ec9b80
mlucas2805 <mlucas.ml27@gmail.com>
2016-07-20 15:31:10 +0200

    Responsive Social Media

--- site/agency/SocialMedia.php
+++ site/agency/SocialMedia.php
@@ -1,4 +1,4 @@
-<div id="menu">
+<div class="menu">
     <ul>
         <li><a class="scroll img-responsive" href="https://github.com/mlucas2805" target="_blank"><img src="public/img/GitHub-50.png" onmouseover="this.src='public/img/GitHub-50.png'" onmouseout="this.src='public/img/GitHub-50.png'" alt="Github"></a></li>
         <li><a class="scroll img-responsive" href="https://fr.linkedin.com/in/marie-lucas-1b6628bb" target="_blank"><img src="public/img/LinkedIn-50.png" onmouseover="this.src='public/img/LinkedIn-50.png'" onmouseout="this.src='public/img/LinkedIn-50.png'" alt="Linkedin"></a></li>
--- site/agency/public/js/script.js
+++ site/agency/public/js/script.js
@@ -3,9 +3,22 @@
  */
 // BOUTONS DE PARTAGE RESEAUX SOCIAUX
 $(window).scroll(function(){
-    if ($(this).scrollTop() > 50) { //niveau d'apparution au scroll
-        $('#menu').fadeIn(300); // vitesse d'apparution
-    } else {
-        $('#menu').fadeOut(300); // vitesse de disparution
+    var windowWidth= $(window).width();
+    if (windowWidth > 1100)
+    {
+        if ($(this).scrollTop() > 50)
+        { //niveau d'apparution au scroll
+            $('.menu').fadeIn(300); // vitesse d'apparution
+        } else
+        {
+            $('.menu').fadeOut(300); // vitesse de disparution
+        }
     }
+    else if(windowWidth < 1100)
+    {
+        $(".menu").hide(".menu");
+    }
+
+
 });
+
